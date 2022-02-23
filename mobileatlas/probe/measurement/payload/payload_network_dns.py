b0339bb107553c161657e2c481d8c02bc0698b08
MobileAtlas Probe (dc:a6:32:ca:bd:cd) <mobileatlas@sba-research.org>
2022-02-23 10:44:30 +0000

    use primary dns only during dns test

--- mobileatlas/probe/measurement/payload/payload_network_dns.py
+++ mobileatlas/probe/measurement/payload/payload_network_dns.py
@@ -13,19 +13,22 @@ class PayloadNetworkDns(PayloadNetworkBase):
 
     def __init__(self, mobile_atlas_mediator: MobileAtlasMediator, payload_size):
         super().__init__(mobile_atlas_mediator, payload_size=payload_size)
-        
+
     def send_payload(self) -> PayloadNetworkResult:
         cnt = 0
         my_resolver = dns.resolver.Resolver()
+        my_resolver._nameservers = my_resolver._nameservers[:1] # just use the very first dns server (that was propagated via dhcp from the provider)
+        #my_resolver._nameservers = my_resolver._nameservers = ['8.8.8.8']
+        logger.info(f"use nameservers {my_resolver._nameservers} for dns payload")
         with open("mobileatlas/probe/measurement/payload/res/tranco_V78N.txt") as file:
             for line in file:
                 domain = line.strip()
                 cnt += 1
                 try:
                     results = my_resolver.resolve(domain, 'A', raise_on_no_answer=False)
-                except (dns.resolver.LifetimeTimeout, dns.resolver.NXDOMAIN, dns.resolver.YXDOMAIN, dns.resolver.NoAnswer) as e:
-                    pass                    
+                except (dns.resolver.LifetimeTimeout, dns.resolver.NXDOMAIN, dns.resolver.YXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers) as e:
+                    pass
                 if self.is_payload_consumed():
                     break
         return PayloadNetworkResult(True, None, *self.get_consumed_bytes(), cnt)
-                
+
