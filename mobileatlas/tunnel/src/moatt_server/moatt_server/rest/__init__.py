c87bbe460354a03540eb600c1df6999851a16b90
Philipp Frenzel <pfrenzel@sba-research.org>
2023-05-08 11:19:31 +0200

    Set isolation level to SERIALIZABLE

--- mobileatlas/tunnel/src/moatt_server/moatt_server/rest/__init__.py
+++ mobileatlas/tunnel/src/moatt_server/moatt_server/rest/__init__.py
@@ -10,6 +10,7 @@ alembic_config.read("alembic.ini")
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = { "isolation_level": "SERIALIZABLE" }
 app.config["SECRET_KEY"] = secrets.token_hex()
 
 db = SQLAlchemy(app)
